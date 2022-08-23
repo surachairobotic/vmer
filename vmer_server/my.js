fffcf1aaf3036c70c953a3cb78836db541500aaf
Surachai Rodsai <surachairobotic@gmail.com>
2022-08-23 14:39:11 +0700

    ...

---
+++ vmer_server/my.js
@@ -0,0 +1,331 @@
+'use strict';
+
+const fs = require('fs');
+var stream = null;
+var logfile_time = null;
+var logfile_name = null;
+const Promise = require('bluebird');
+const del = require('del');
+var child_process = require('child_process');
+var PATH = require('path');
+
+module.exports = {
+  init: function(options = {}){
+    logfile_time = get_time(this.Date());
+    module.exports.config = {
+      "port":8085,
+      "port_udp":8086,
+      "tmp_dir":PATH.resolve("./tmp/"),
+      "log_dir":PATH.resolve("./log/"),
+      "result_dir":PATH.resolve("./data/"),
+      "route_dir":PATH.resolve("./route/"),
+    };
+    return check_dir_exist(module.exports.config.log_dir)
+    .then(()=>{
+      clear_old_files(module.exports.config.log_dir, 30);
+      process.env.NODE_ENV = 'production';
+      stream = fs.createWriteStream( get_logfile_name(logfile_time), {flags:'a'});
+      module.exports.log('info','port = '+module.exports.config['port']);
+      module.exports.log('info','log = '+logfile_name);
+      return check_dir_exist(module.exports.config.result_dir)
+      .then(()=>{
+        return check_dir_exist(module.exports.config.route_dir);
+      }).then(()=>{
+        return check_dir_exist(module.exports.config.tmp_dir);
+      }).then(()=>{
+        return remove_old_log_files();
+      });
+    });
+  },
+
+  get_time: get_time,
+  get_time_str: get_time_str,
+
+  log: function(level, msg, msg2){
+    var str = get_log_str(level, msg, msg2);
+    console.log(str);
+    if( !stream )
+      return;
+    stream.write( str + '\n');
+  },
+
+  end: function(){
+    if( stream ){
+      stream.end();
+      stream = null;
+    }
+    if( timer_clear_cache )
+      clearInterval(timer_clear_cache);
+  },
+
+  log_wait: function(level, cb, msg, msg2){
+    if( !stream ){
+      cb();
+      return;
+    }
+    var str = get_log_str(level, msg, msg2);
+    console.log(str);
+    stream.write( str + '\n', cb);
+  },
+
+  log_sync: function(level, msg, msg2){
+    if( stream ){
+      stream.end();
+      stream = null;
+    }
+    var str = get_log_str(level, msg, msg2);
+    console.log(str);
+    fs.appendFileSync( logfile_name, str + '\n' );
+  },
+
+  remove_old_log_files: remove_old_log_files,
+
+  is_release: function(){
+    return this.config.app_mode=='release';
+  },
+
+  check_dir_exist: check_dir_exist,
+  get_logfile_name: get_logfile_name,
+  create_random_string: create_random_string,
+
+  isNaN: function(n){
+    return n===null || typeof(n) == 'undefined' || isNaN(n) || n==='';
+  },
+
+  // not safe. can use glob pattern (*)
+  // not work with the name with parenthesis like "data copy (2)"
+  del: function(path, b_check_deleted = true){
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        del( path, {force:true,dryRun:false})
+        .then((eles)=>{
+          if( b_check_deleted && eles.length!=path.length ){
+            module.exports.log('warn', 'my.del : cannot remove : '+path);
+            reject();
+          }
+          else
+            resolve();
+        });
+      }, 100);
+    });
+  },
+
+  del_dir: function( dirs, b_check_deleted = true){
+    if( !Array.isArray(dirs) ){
+      dirs = [dirs];
+    }
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        Promise.all( dirs.map((d)=>{
+          return fs.promises.rmdir( d, { recursive: true } )
+          .then(resolve)
+          .catch((e)=>{
+            if( b_check_deleted )
+              return reject('e');
+          });
+        }));
+      }, 100);
+    });
+  },
+
+  move_file: function(src, dst){
+    module.exports.log('info', 'move_file: '+src+' => '+dst);
+    return fs.promises.copyFile( src, dst )
+    .then(()=>{
+      return fs.promises.unlink( src );
+    });
+  },
+
+  copy_all: function(src, dst){
+    return fs.promises.readdir(src, {withFileTypes: true})
+    .then((files)=>{
+      return Promise.all( files.map((f)=>{
+        if( f.isFile() ){
+          return fs.promises.copyFile( PATH.join(src,f.name), PATH.join(dst,f.name) );
+        }
+        else{
+          return check_dir_exist( PATH.join(dst,f.name) )
+          .then(()=>{
+            return module.exports.copy_all( PATH.join(src,f.name), PATH.join(dst,f.name) );
+          });
+        }
+      }));
+    });
+  },
+
+  Date: function(param){
+    return param ? new Date(param) : new Date();
+  },
+
+  alert: function(msg,caption='Alert', b_sync=false){
+    var cmd = "powershell.exe", arg = [`Add-Type -AssemblyName PresentationCore,PresentationFramework;[System.Windows.MessageBox]::Show('${msg}','${caption}');`];
+    if(b_sync)
+      child_process.spawnSync( cmd, arg );
+    else
+      child_process.spawn( cmd, arg );
+  }
+
+};
+
+function get_time(currentdate){
+  return {
+    year: currentdate.getFullYear()-2000,
+    month: currentdate.getMonth() + 1,
+    date: currentdate.getDate(),
+    day: currentdate.getDay(),
+    hour: currentdate.getHours(),
+    minute: currentdate.getMinutes(),
+    second: currentdate.getSeconds()
+  };
+}
+
+function get_logfile_name(t=null){
+  if( !t )
+    t = logfile_time;
+  //logfile_name = PATH.join( module.exports.config.log_dir, String(t.day)+".log" );
+  logfile_name = PATH.join( module.exports.config.log_dir, String(t.year) + (t.month<10 ? '0' : '') + t.month
+    + (t.date < 10 ? ('0' + t.date) : t.date)
+    + ".log");
+  return logfile_name;
+}
+
+
+function get_time_str(t){
+  return String(t.year)
+    + (t.month<10 ? '0' : '') + t.month
+    + (t.date < 10 ? ('0' + t.date) : t.date) + '-'
+    + (t.hour < 10 ? ('0' + t.hour) : t.hour)
+    + (t.minute < 10 ? ('0' + t.minute) : t.minute)
+    + (t.second < 10 ? ('0' + t.second) : t.second);
+}
+
+
+
+function get_log_str(level, msg, msg2){
+  var t = get_time(module.exports.Date());
+
+  var fn = function(s){
+    if( !s )
+      return '';
+    if( typeof(s)=='string' )
+      return s;
+    if( s.toString )
+      return s.toString().replace('\\n', '\n');
+    return JSON.stringify(s);
+  };
+  var e = '';
+  if( level=='error' || level=='warn' ){
+    e+='\n';
+    if( msg && msg.stack )
+      e+= msg.stack;
+    else if( msg2 && msg2.stack )
+      e+= msg2.stack;
+    else if( level=='error' )
+      e+= (new Error()).stack;
+  }
+  return get_time_str(t)
+    + ',' + level + ','
+    + fn(msg)
+    + fn(msg2)
+    + e;
+}
+
+function remove_old_log_files(){
+  const MAX_FILE_NUM = 7;
+  return fs.promises.readdir(module.exports.config.log_dir)
+  .then((files)=>{
+    if( files.length>MAX_FILE_NUM ){
+      files = files.map((f)=>PATH.join(module.exports.config.log_dir,f));
+      files.sort(function(a, b) {
+        return fs.statSync(a).mtime.getTime() -
+          fs.statSync(b).mtime.getTime();
+      });
+      for(var i=0;i<files.length-MAX_FILE_NUM;i++){
+        fs.promises.unlink( files[i] );
+      }
+    }
+  });
+}
+
+
+function check_dir_exist(dir){
+  return fs.promises.access( dir, fs.constants.F_OK )
+  .catch((e)=>{
+    if( e.code === "ENOENT"){
+      return fs.promises.mkdir( dir )
+      .then(()=>{
+        module.exports.log('info', 'dir created : '+dir);
+      });
+    }
+    else{
+      module.exports.log('error', 'check_dir_exist() : fs.access failed '+dir, e);
+      return Promise.reject(e);
+    }
+  });
+}
+
+function clear_old_files(dir, day){
+  var now = new Date().getTime();
+  return fs.promises.readdir( dir )
+  .then((files)=>{
+    return Promise.all( files.map((file)=>{
+      return fs.promises.stat(PATH.join(dir, file))
+      .then((stat)=>{
+        var endTime;
+        endTime = new Date(stat.mtime).getTime() + day*24*3600000;
+        if (now > endTime) {
+          fs.unlink(PATH.join(dir, file), function(err) {
+            if (err) {
+              module.exports.log('error', 'clear_old_files() : fs.unlink failed : '+PATH.join(dir, file), err);
+            }
+            module.exports.log('info', 'clear_old_files() : remove : '+PATH.join(dir, file));
+          });
+          return Promise.resolve();
+        }
+      });
+    }));
+  });
+  /*
+  fs.readdir( dir, function(err, files) {
+    if ( err || !files ) {
+      return;
+    }
+    files.forEach(function(file, index) {
+      fs.stat(PATH.join(dir, file), function(err, stat) {
+        var endTime;
+        if (err) {
+          try{
+            module.exports.log('error', 'clear_old_files() : fs.stat failed : '+PATH.join(dir, file), err);
+          }
+          catch(e){}
+          return;
+        }
+        endTime = new Date(stat.mtime).getTime() + day*24*3600000;
+        if (now > endTime) {
+          fs.unlink(PATH.join(dir, file), function(err) {
+            try{
+              if (err) {
+                module.exports.log('error', 'clear_old_files() : fs.unlink failed : '+PATH.join(dir, file), err);
+              }
+              module.exports.log('info', 'clear_old_files() : remove : '+PATH.join(dir, file));
+            }
+            catch(e){}
+          });
+        }
+      });
+    });
+  });*/
+}
+
+function create_random_string(len){
+  const possible = "abcdefghijklmnpqrstuvwxyz123456789";
+  var text = "";
+  for (var i = 0; i < len; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
+
+
+if (require.main === module) {
+  module.exports.init();
+}
---
+++ vmer_server/run_vmer.js
@@ -0,0 +1,492 @@
+'use strict';
+
+const Promise = require('bluebird');
+const express = require('express');
+var http = require('http');
+var app = express();
+const bodyParser = require('body-parser');
+var multer  = require('multer');
+var multer_upload = null;
+var PATH = require('path');
+var archiver = require('archiver');
+var extract = require('extract-zip');
+var my = require('./my');
+var fs = require('fs');
+var dgram = require('dgram').createSocket('udp4');
+const child_process = require('child_process');
+
+var server = http.createServer(function (req, res) {
+  app(req,res);
+});
+
+function init(){
+  try{
+    my.init();
+    multer_upload = multer({ dest: my.config.tmp_dir });
+    init_web();
+    init_udp();
+    server.on('error', function (e) {
+      my.log('warn', e);
+    });
+    server.on('listening', function (e) {
+      for(var i = 0;i < process.argv.length; i++){
+        if( process.argv[i]=='show_start_popup' ){
+          my.alert('The program started properly','Info');
+          break;
+        }
+      }
+    });
+    server.listen( my.config.port, '0.0.0.0');
+  }
+  catch(e){
+    try{
+      my.log('warn', e);
+      if( typeof(e)=='string' ){
+        e+= '\n'+'See more detailes in : '+PATH.resolve(my.get_logfile_name());
+      }
+    }
+    catch(e){}
+    my.alert(e);
+  }
+}
+
+// for discovering devices
+function init_udp(){
+  dgram.on('listening', function () {
+    var address = dgram.address();
+    my.log('info','UDP Client listening on ' + address.address + ":" + address.port);
+  });
+
+  dgram.on('message', function (msg, rinfo) {
+    my.log('info','Message from: ' + rinfo.address + ':' + rinfo.port +' - ' + msg);
+    if(msg=='discover_devices'){
+      return get_device_name()
+      .then((device_name)=>{
+        var res = Buffer.from(JSON.stringify({
+          result: 'success',
+          device_id: my.config.device_id,
+          device_name: device_name,
+        }));
+        dgram.send( res, 0, res.length, rinfo.port, rinfo.address, ()=>{
+          my.log('info', 'discover_devices sent back');
+        });
+      });
+    }
+  });
+
+  dgram.on('error', function (e) {
+    if( e.message.indexOf('EADDRINUSE') ){
+      my.alert('Port '+my.config.port_udp+' already in use.\nThe program may already be running.');
+    }
+  });
+
+  try{
+    dgram.bind(my.config.port_udp);
+  }
+  catch(e){
+    my.alert(e, true);
+    process.exit();
+  }
+}
+
+function init_web(){
+  app.set('port', my.config.port);
+  app.use(bodyParser.urlencoded({
+      extended: true
+  }));
+  app.use(bodyParser.json());
+
+  app.get('/api/:api', function(req,res){
+    var data;
+    if( typeof(req.query.data)=='string' ){
+      try{
+        data = JSON.parse(req.query.data);
+      }
+      catch(err){
+        console.log(err);
+        data = null;
+      }
+    }
+    else{
+      data = req.query.data;
+    }
+    return process_api( req, res, req.params.api, data )
+    .catch((e)=>{
+      api_error(req, res, e);
+    });
+  });
+
+  app.post('/api/:api', multer_upload.single('file'), function (req, res, next) {
+    my.log('info','post : '+req.params.api);
+    var arr = req.file.originalname.split('.');
+    if( arr[arr.length-1]!='zip' ){
+      api_error(req, res, 'post : Invalid file ext:'+req.file.originalname);
+      return;
+    }
+    return extract_zip( req.file.path )
+    .then((ret)=>{
+      if( req.params.api=='upload_routes' ){
+        //return my.del(my.config.route_dir, false)
+        //.then(()=>{
+        return check_exist(my.config.route_dir)
+        .then((b_exist)=>{
+          if( b_exist )
+            return Promise.resolve();
+          else
+            return fs.promises.mkdir(my.config.route_dir);
+        }).then(()=>{
+          return my.copy_all( ret.dir, my.config.route_dir );
+        }).then(()=>{
+          return get_route_list();
+        }).then((routes)=>{
+          res.send({
+            result: 'success',
+            routes: routes,
+          });
+          my.log('info','upload_route completed');
+        }).finally(()=>{
+          return my.del_dir(ret.dir, false);
+        });
+      }
+      else{
+        return Promise.reject('Invalid post api name : '+req.params.api);
+      }
+    })
+    .catch((e)=>{
+      api_error(req, res, e);
+    })
+    .finally(()=>{
+      fs.promises.unlink(req.file.path);
+    });
+  });
+}
+
+
+// send error message back to client if error with API
+function api_error(req, res, err){
+  var msg = (err && typeof(err)=='string') ? err : 'Unknown error';
+  res.send({
+    result: 'fail',
+    msg: msg
+  });
+  if( err )
+    console.log('api error : ',err);
+};
+
+
+function process_api( req, res, api_name, data ){
+  if( api_name=='get_device_info' ){
+    return get_device_name()
+    .then((device_name)=>{
+      res.send({
+        result: 'success',
+        device_id: my.config.device_id,
+        device_name: device_name,
+      });
+    });
+  }
+  else if( api_name=='get_route_list' ){
+    return get_route_list()
+    .then((routes)=>{
+      res.send({
+        result: 'success',
+        routes: routes,
+      });
+    });
+  }
+  else if( api_name=='remove_routes' ){
+    my.log('info','remove_route : ',data.route_names);
+    if( !data.route_names ){
+      my.log('warn','remove_route : no route name');
+      return Promise.reject('No route name');
+    }
+
+    // remove all routes
+    if( data.route_names=='*' ){
+      return my.del_dir(my.config.route_dir, true)
+      .then(()=>{
+        return check_exist(my.config.route_dir);
+      }).then((ret)=>{
+        if( ret )
+          return Promise.resolve();
+        else
+          return fs.promises.mkdir(my.config.route_dir);
+      }).then(()=>{
+        return get_route_list();
+      }).then((routes)=>{
+        res.send({
+          result: 'success',
+          routes: routes,
+        });
+      });
+    }
+    else if( !Array.isArray(data.route_names) ){
+      my.log('warn','remove_route : route_names is not an array');
+      return Promise.reject('Route names are not in an array');
+    }
+
+    var route_dir = PATH.resolve( my.config.route_dir );
+    return Promise.all( data.route_names.map( route_name =>{
+      if( typeof(route_name)!='string' ){
+        my.log('warn','remove_route : route name is not a string : ',route_name);
+        return Promise.reject('Route name is not a string : '+JSON.stringify(route_name));
+      }
+      let path = PATH.resolve(PATH.join( route_dir, route_name ));
+      if( route_name.indexOf('..')>=0
+          || path.indexOf(route_dir)<0
+          || path==route_dir ){
+        my.log('warn','remove_route : invalid route name : ',route_name);
+        return Promise.reject('Invalid route name : '+route_name);
+      }
+      return check_exist(path)
+      .then((ret)=>{
+        if( !ret ){
+          my.log('warn','remove_route : route not exist');
+          return Promise.reject('The route does not exist : '+route_name);
+        }
+      });
+    })).then(()=>{
+      return Promise.all( data.route_names.map( route_name =>{
+        return my.del_dir( PATH.resolve(PATH.join( route_dir, route_name )) );
+      }));
+    }).then(()=>{
+      return get_route_list();
+    }).then((routes)=>{
+      res.send({
+        result: 'success',
+        routes: routes,
+      });
+    });
+    /*
+    var path = PATH.resolve(PATH.join( route_dir, data.route_name ));
+//    console.log(route_dir,path);
+    if( data.route_name.indexOf('..')>=0
+        || path.indexOf(route_dir)<0
+        || path==route_dir ){
+      my.log('warn','remove_route : invalid route name');
+      return Promise.reject('Invalid route name');
+    }
+    return check_exist(path)
+    .then((ret)=>{
+      if( !ret ){
+        my.log('warn','remove_route : route not exist');
+        return Promise.reject('The route does not exist');
+      }
+      return my.del( path );
+    }).then(()=>{
+      res.send({
+        result: 'success'
+      });
+    });*/
+  }
+  else if( api_name=='get_data_list' ){
+    return fs.promises.readdir( my.config.result_dir )
+    .then((files)=>{
+      res.send({
+        result: 'success',
+        data_names: files,
+      });
+    });
+  }
+  else if( api_name=='download_data' ){
+    var tmp_file = null, files = [];
+    var fn_gen_tmp_file_path = function(cnt=0){
+      var path = PATH.join(my.config.tmp_dir, my.create_random_string(10)+'.zip');
+      return check_exist(path)
+      .then((ret)=>{
+        if( ret ){
+          if( cnt > 10 ){
+            my.log('warn', 'fn_gen_tmp_file_path : too many attemp : '+path);
+            return Promise.reject();
+          }
+          return fn_gen_tmp_file_path(cnt+1);
+        }
+        else
+          return Promise.resolve(path);
+      });
+    };
+
+    return fn_gen_tmp_file_path()
+    .then((path)=>{
+      tmp_file = path;
+      return fs.promises.readdir( my.config.result_dir );
+    }).then((_files)=>{
+      //files = _files;
+
+      if( data.data_names=='*' ){
+        files = _files;
+      }
+      else if( !Array.isArray(data.data_names) ){
+        my.log('warn', 'download_data : data_names is not an array : '+JSON.stringify(data.data_names));
+        return Promise.reject('Data names are not in an array');
+      }
+      else{
+        files = data.data_names;
+        for(var i=0;i<files.length;i++){
+          if( !_files.includes(files[i]) ){
+            my.log('warn', 'download_data : file not found : '+files[i]);
+            return Promise.reject('Data file not found : '+files[i]);
+          }
+        }
+      }
+
+      if( files.length==0 ){
+        my.log('info', 'download_data : no result file');
+        res.status(204).send('No result file');
+        return Promise.resolve();
+      }
+      return new Promise((resolve,reject)=>{
+        var output = fs.createWriteStream(tmp_file);
+        var archive = archiver('zip');
+
+        archive.on('error', function(err){
+          reject(err);
+        });
+        archive.pipe(output);
+        files.forEach((f)=>{
+          //archive.file(PATH.join(my.config.result_dir, f), {name: f});
+          archive.directory(PATH.join(my.config.result_dir, f), f);
+        });
+
+        output.on("close", function(){
+          resolve();
+        });
+        archive.finalize();
+      }).then(()=>{
+        return new Promise((resolve,reject)=>{
+          res.download(tmp_file, 'results.zip', (err)=>{
+            if (err) {
+              my.log('warn', err);
+              reject();
+              return;
+            }
+            my.log('info', 'download_data : file sent');
+            my.del( tmp_file, false );
+            //my.del( files.map( f => PATH.join(my.config.result_dir, f) ), false );
+            resolve();
+          });
+        });
+      });
+    })
+    .catch((e)=>{
+      try{
+        my.log('warn', e);
+        if( tmp_file )
+          my.del(tmp_file,false);
+        res.status(500).send({result: 'fail', msg: typeof(e)=='string' ? e : 'Unexpected error'});
+      }
+      catch(e){
+        my.log('warn', e);
+      }
+    });
+  }
+  else if( api_name=='remove_data' ){
+    var files = [];
+    return fs.promises.readdir( my.config.result_dir )
+    .then((_files)=>{
+      if( data.data_names=='*' ){
+        files = _files;
+      }
+      else if( !Array.isArray(data.data_names) ){
+        my.log('warn', 'remove_data : data_names is not an array : '+JSON.stringify(data.data_names));
+        return Promise.reject('Data names are not in an array');
+      }
+      else{
+        files = data.data_names;
+        for(var i=0;i<files.length;i++){
+          if( !_files.includes(files[i]) ){
+            my.log('warn', 'remove_data : file not found : '+files[i]);
+            return Promise.reject('Data not found : '+files[i]);
+          }
+        }
+      }
+      if( files.length==0 ){
+        my.log('info', 'remove_data : no result file');
+        return Promise.reject('No data');
+      }
+      return my.del_dir( files.map( f => PATH.join(my.config.result_dir, f) ), true );
+    }).then(()=>{
+      return process_api( req, res, 'get_data_list', null );
+    });
+  }
+  else{
+    return Promise.reject('invalid get api : '+api_name);
+  }
+}
+
+
+function extract_zip(src, dst, cnt=0){
+  var dir = PATH.join(my.config.tmp_dir, my.create_random_string(12));
+  return check_exist(dir)
+  .then((ret)=>{
+    if( ret ){
+      if( cnt < 10 )
+        return resolve( extract_zip( src, dst, cnt+1 ) );
+      else{
+        my.log('error', 'extract_zip : too many attemp : src = '+src+', dst = '+dst);
+        return reject();
+      }
+    }
+    return extract( src, {dir: dir} );
+  }).then(()=>{
+    return fs.promises.readdir(dir);
+  }).then((files)=>{
+    return Promise.resolve({'dir': dir, 'files': files.map(f=>PATH.join(dir,f))});
+  })
+  .catch((e)=>{
+    my.del_dir(dir, false);
+    return Promise.reject(e);
+  });
+}
+
+function get_device_name(){
+  //wmic.exe useraccount where "localaccount=true" get fullname
+  return new Promise((resolve,reject)=>{
+    var proc = child_process.execFile('wmic.exe', ['useraccount', 'where','localaccount=true','get','fullname'], {windowsHide : true}, (err, stdout, stderr) => {
+      if (err) {
+        my.log('error', 'get_username : ',err);
+        return resolve('Cannot get device name');
+      }
+      var arr = stdout.split('\r\n');
+      for(var i=0;i<arr.length;i++){
+        var s = arr[i].trim();
+        if( s && s!='FullName' ){
+          return resolve(s);
+        }
+      }
+      return resolve('No device name');
+    });
+  });
+}
+
+function get_route_list(){
+  return check_exist(my.config.route_dir)
+  .then((ret)=>{
+    if( ret ){
+      return fs.promises.readdir( my.config.route_dir, {withFileTypes: true} )
+      .then((files)=>{
+        return Promise.resolve(files.filter(f => f.isDirectory()).map(f => f.name));
+      });
+    }
+    else{
+      return Promise.resolve([]);
+    }
+  });
+}
+
+function check_exist(src){
+  return fs.promises.access( src, fs.constants.F_OK )
+  .then(()=>{
+    return Promise.resolve(true);
+  })
+  .catch((e)=>{
+    if( e.code === "ENOENT")
+      return Promise.resolve(false);
+    else
+      return Promise.reject(e);
+  });
+}
+
+if (require.main === module) {
+  init();
+}
